1b3954fb0e352ffb21d3c198f4b7ed2685612af1
frankwear <78041385+frankwear@users.noreply.github.com>
2023-06-28 19:22:21 -0400

    Create Location.java

---
+++ Location.java
@@ -0,0 +1,21 @@
+public class Location {
+    double longitude;
+    double latitude;
+
+
+    public Location (double la, double lo) {
+        this.longitude = lo;
+        this.latitude = la;
+    }
+
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+
+}
