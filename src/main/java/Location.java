a22505f8ba8b26943d7809fda4ac082df30ceed9
frankwear <78041385+frankwear@users.noreply.github.com>
2023-07-07 18:36:20 -0400

    Update Location.java
    Added isMatch method

--- src/main/java/Location.java
+++ src/main/java/Location.java
@@ -9,7 +9,15 @@ public class Location {
         this.longitude = lo;
         this.latitude = la;
     }
-
+    public boolean isMatch(Location tempLoc) {
+        // match rounded to 4 decimal places - about 10 meter, so 20 meter square box around location
+        if (Math.round(tempLoc.getLatitude() * 10000) == Math.round(this.getLatitude() * 10000)){
+            if (Math.round(tempLoc.getLongitude() * 10000) == Math.round(this.getLongitude() * 10000)){
+                return Boolean.TRUE;
+            }
+        }
+        return Boolean.FALSE;
+    }
 
     public double getLongitude() {
         return longitude;
